05c8f1c49c42da9a13f5fd5be193dcc6cf4e96a1
sekyungk <sekyungk@outlook.com>
2021-06-25 13:26:43 +0900

    test

--- .github/actions/my-script.js
+++ .github/actions/my-script.js
@@ -2,18 +2,18 @@ const { Octokit } = require("@octokit/action");
 
 const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
-console.log(process.env);
+const { TAG_NAME } = process.env;
 
 myAsyncMethod();
 
 async function myAsyncMethod() {
   const { data } = await octokit.request(
-    "GET /repos/{owner}/{repo}/releases/tags/{tag_name}",
+    "GET /repos/{owner}/{repo}/releases/tags/{TAG_NAME}",
     {
       owner,
       repo,
-      tag_name: "1.38.0",
+      TAG_NAME,
     }
   );
-  console.log("Issue created: %s", data);
+  console.log(data.id);
 }
