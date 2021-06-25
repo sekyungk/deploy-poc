5c0cf3a4ae9f3d1947b75935126e6a778fbe8b9f
sekyungk <sekyungk@outlook.com>
2021-06-25 14:33:11 +0900

    test

--- .github/actions/delete-release.js
+++ .github/actions/delete-release.js
@@ -4,27 +4,23 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 const { TAG_NAME } = process.env;
 
-deleteReleaseTag();
-
-async function deleteReleaseTag() {
-  const { data: releases } = await octokit.request(
-    "GET /repos/{owner}/{repo}/releases/tags/{tag}",
-    {
-      owner,
-      repo,
-      tag: TAG_NAME,
-    }
-  );
-
-  await octokit.request("Delete /repos/{owner}/{repo}/releases/{release_id}", {
-    owner,
-    repo,
-    release_id: releases.id,
-  });
-
-  await octokit.request("Delete /repos/{owner}/{repo}/git/refs/tags/{tag}", {
+const { data: releases } = await octokit.request(
+  "GET /repos/{owner}/{repo}/releases/tags/{tag}",
+  {
     owner,
     repo,
     tag: TAG_NAME,
-  });
-}
+  }
+);
+
+await octokit.request("Delete /repos/{owner}/{repo}/releases/{release_id}", {
+  owner,
+  repo,
+  release_id: releases.id,
+});
+
+await octokit.request("Delete /repos/{owner}/{repo}/git/refs/tags/{tag}", {
+  owner,
+  repo,
+  tag: TAG_NAME,
+});
