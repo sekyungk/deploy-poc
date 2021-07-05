fb18a5479fcdd83947c8b4a4b6e9238b733f4fc7
sekyungk <sekyungk@outlook.com>
2021-07-05 20:48:48 +0900

    WEBP-202 commit message

--- .github/actions/update-ref.js
+++ .github/actions/update-ref.js
@@ -7,31 +7,30 @@ const { deployment, sha } = process.env;
 updateDeploymentRef();
 
 async function updateDeploymentRef() {
-  const { data: deploymentRef } = await octokit.request(
-    "GET /repos/{owner}/{repo}/git/ref/deployments/{deployment}",
-    {
-      owner,
-      repo,
-      deployment,
-    }
-  );
-
-  if (!deploymentRef) {
-    return await octokit.request("POST /repos/{owner}/{repo}/git/refs", {
+  try {
+    await octokit.request(
+      "GET /repos/{owner}/{repo}/git/ref/deployments/{deployment}",
+      {
+        owner,
+        repo,
+        deployment,
+      }
+    );
+    await octokit.request(
+      "PATCH /repos/{owner}/{repo}/git/refs/deployments/{deployment}",
+      {
+        owner,
+        repo,
+        deployment,
+        sha,
+      }
+    );
+  } catch (e) {
+    await octokit.request("POST /repos/{owner}/{repo}/git/refs", {
       owner,
       repo,
       ref: `refs/deployments/${deployment}`,
       sha,
     });
   }
-
-  return await octokit.request(
-    "PATCH /repos/{owner}/{repo}/git/refs/deployments/{deployment}",
-    {
-      owner,
-      repo,
-      deployment,
-      sha,
-    }
-  );
 }
